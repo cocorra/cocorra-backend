a93f4b461dfb2029a0d5ccb6839a2f53533c7291
Kareem Elawamy <kareemalwamy@gmail.com>
2026-08-23 15:39:35 +0300

    fix img room bug

--- Cocorra.BLL/Services/UploadService/IUploadImage.cs
+++ Cocorra.BLL/Services/UploadService/IUploadImage.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 
 namespace Cocorra.BLL.Services.Upload
 {
     public interface IUploadImage
     {
-        Task<string> SaveImageAsync(IFormFile imageFile);
+        Task<string> SaveImageAsync(IFormFile imageFile, string subFolder = "Profiles");
         void DeleteImage(string? imagePath); 
     }
 }
